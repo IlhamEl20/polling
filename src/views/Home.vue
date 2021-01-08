7522849788f06d9fa72e37b885d70516dbb0e034
Squel <toyepadam@gmail.com>
2021-01-08 15:52:08 +0700

    coba

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+   
+     <section class="vote">
+ 	<h3 class="give-your-vote">Silahkan berikan penilaian anda terhadap pelayanan kami
+ 	</h3>
+ 	<div class="emoticons">
+ 		//komenet pote
+ 	</div>
+ </section>
+<section class="finish">
+	<h4>Terimakasih atas penilainyan</h4>
+</section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    //HelloWorld
+  }
+};
+</script>
+<style type="text/css">
+	.container{
+		display: flex;
+		flex-wrap: wrap;
+		height: 100vh;
+		align-items: center;
+	}
+	section{
+		width: 100%;
+	}
+</style>
